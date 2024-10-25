d2beec648365318cb49ee434c976c98c25a1cdef
valencsoares <pvcampossoares558@gmail.com>
2024-10-25 15:13:54 -0400

    Update Home with the components

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,16 @@
+import Menu from "./componentes/Menu.jsx";
+import Card from "./componentes/card.jsx";
+import Navbar from "./componentes/navbar.jsx";
+
 export default function Home(){
+    const listaVideosSalva = JSON.parse(localStorage.getItem("ListaVideos"))|| [];
     return(
         <body>         
             <Navbar/>  
             <div className="container">
                 <Menu/>
-                <div className="cards"> <p>card</p>
+                <div className="cards">
+                    {listaVideosSalva.map((ativ) => <Card ativ = {ativ}/>)}
                 </div>
             </div>
         </body> 
