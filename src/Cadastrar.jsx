6d3ca44e8c46d62760dc5ce4baa8710f124c2dfc
valencsoares <pvcampossoares558@gmail.com>
2024-10-25 15:18:19 -0400

    Do the necessaries functions to add new videos

--- src/Cadastrar.jsx
+++ src/Cadastrar.jsx
@@ -7,8 +7,35 @@ import {Link, useNavigate} from "react-router-dom";
 
 export default function Cadastrar(){
     const listaVideosSalva = JSON.parse(localStorage.getItem("ListaVideos"))|| [];   
+    const [listaVideos, setListaVideos] = useState(listaVideosSalva);
+    const [url, setURL] = useState("");
+    const [canal, setCanal] = useState("");
+    const [tempo, setTempo] = useState("");
+    const [data, setData] = useState("");
+    const [titulo, setTitulo] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [id, setId] = useState(listaVideosSalva [listaVideosSalva.length - 1]?.id + 1 || 1);
+    /* estados */
+
     useEffect(() => { localStorage.setItem("ListaVideos", JSON.stringify(listaVideos)) }, [listaVideos]);
     const navigate = useNavigate();
+
+    const salvar = async (e) =>{
+        e.preventDefault(); //não recarregar a página
+        await setListaVideos([...listaVideos, { //"..." serve para pegar tudo que tem na lista e add algo a mais
+            url:url, canal:canal, tempo:tempo, data:data, titulo:titulo, descricao:descricao, id:id
+        }]); 
+        //para fazer com que apague as informações do input quando chamar a função
+        setURL("")
+        setCanal("")
+        setTempo("")
+        setData("")
+        setTitulo("")
+        setDescricao("")
+        setId(id + 1)
+        navigate("/") //acionar a pagina da home
+    };
+
     return(
         <div className="container">
             <Navbar/>
