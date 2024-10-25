b8060ea24f7014d121fd7201544ae50a5fd0ac75
valencsoares <pvcampossoares558@gmail.com>
2024-10-25 15:07:11 -0400

    feito a base da Home

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home(){
+    return(
+        <body>         
+            <Navbar/>  
+            <div className="container">
+                <Menu/>
+                <div className="cards"> <p>card</p>
+                </div>
+            </div>
+        </body> 
+     );
+}
